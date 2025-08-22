cb6da97f136a41f8561eedb25fc5bd10d6c6ed0c
Prajjwal Gupta <uj7860951375@gmail.com>
2025-08-23 00:02:06 +0530

    Added initial Java program file

---
+++ first.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class PehliKakshaz {
+    public static void main(String args[])
+    {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter first number: ");
+       int num1 = sc.nextInt();
+       System.out.print("Enter second number: ");
+       int num2 = sc.nextInt();
+       int sum = num1+num2;
+       System.out.println("Sum of the two numbers that you entered wiz. "+num1+" and "+num2+" is : "+sum);
+    }
+}
